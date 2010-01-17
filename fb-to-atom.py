bc65f4c1c81bfbd8f7d8187facbaa17afe2ebad0
Lars Kellogg-Stedman <lars@oddbit.com>
2010-01-17 13:42:26 -0500

    load app credentials from file

--- fb-to-atom.py
+++ fb-to-atom.py
@@ -3,15 +3,15 @@ import sys
 import datetime
 import time
 import cPickle as pickle
+import optparse
+from ConfigParser import ConfigParser
 
 import facebook
 import PyRSS2Gen as RSS2
 
 from xml.etree import ElementTree as ET
 
-FB_API_KEY      = '1d35ab34c58ef4571acbef5e0f119686'
-FB_SECRET_KEY   = 'ccf876e02225c98dc071f08e8fdeaf47'
-
+NS_ATOM         = 'http://www.w3.org/2005/Atom'
 CATEGORY_KIND   = 'http://schemas.google.com/g/2005#kind'
 POST_KIND       = 'http://schemas.google.com/blogger/2008/kind#post'
 
@@ -19,6 +19,9 @@ CCACHE          = os.path.join(os.environ['HOME'], '.fbexportnotes')
 
 class ApplicationError(Exception):
     pass
+
+def atomelement(e):
+    return '{%s}%s' % (NS_ATOM, e)
 
 def fmtTime(t):
     return time.strftime('%Y-%m-%dT%H:%M:%S.000-05:00', time.localtime(t))
@@ -33,8 +36,28 @@ def addSubElement(element, name, attrs=None, text=None):
 
     return x
 
+def parse_args():
+    p = optparse.OptionParser()
+    p.add_option('-c', '--credentials',
+            help='File from which to read Facebook application credentials.')
+
+    return p.parse_args()
+
 def main():
+    opts, args = parse_args()
+    config = ConfigParser()
+
+    if opts.credentials:
+        config.read(opts.credentials)
+
+    FB_API_KEY = config.get('facebook', 'api key')
+    FB_SECRET_KEY = config.get('facebook', 'secret key')
+
+    print 'API KEY:', FB_API_KEY
+    print 'SECRET KEY:', FB_SECRET_KEY
+
     fb = facebook.Facebook(FB_API_KEY, FB_SECRET_KEY)
+    return fb
 
     if os.path.exists(CCACHE):
         auth = pickle.load(open(CCACHE))
@@ -42,7 +65,7 @@ def main():
         fb.secret = auth['secret']
         fb.uid = auth['uid']
     else:
-        fb.auth.createToken()
+        tok = fb.auth.createToken()
         fb.login()
         auth = fb.auth.getSession()
         pickle.dump(auth, open(CCACHE, 'w'))
@@ -53,31 +76,33 @@ def main():
     notes = fb.fql.query('SELECT note_id, created_time, updated_time, title, content FROM note WHERE uid=%d' % fb.uid)
     name = fb.users.getInfo(fb.uid, 'first_name')[0]['first_name']
 
-    feed = ET.Element('{http://www.w3.org/2005/Atom}feed')
+    feed = ET.Element(atomElement('feed'))
 
-    addSubElement(feed, '{http://www.w3.org/2005/Atom}title', attrs={'type': 'text'}, 
+    addSubElement(feed, atomElement('title'),
+            attrs={'type': 'text'}, 
             text='Notes for %s' % name)
-    addSubElement(addSubElement(feed, '{http://www.w3.org/2005/Atom}author'),
-            '{http://www.w3.org/2005/Atom}name', text=name)
-    addSubElement(feed, '{http://www.w3.org/2005/Atom}generator',
+    addSubElement(addSubElement(feed, atomElement('author')),
+            atomElement('name'),
+            text=name)
+    addSubElement(feed, atomElement('generator'),
             text='Blogger')
 
     for note in notes:
-        entry = addSubElement(feed, '{http://www.w3.org/2005/Atom}entry')
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}id',
+        entry = addSubElement(feed, atomElement('entry'))
+        addSubElement(entry, atomElement('id'),
                 text='http://www.facebook.com/notes.php?id=%s' % note['note_id'])
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}title',
+        addSubElement(entry, atomElement('title'),
                 attrs={'type': 'text'},
                 text=note['title'])
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}category',
+        addSubElement(entry, atomElement('category'),
                 attrs={'scheme': CATEGORY_KIND, 'term': POST_KIND})
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}published',
+        addSubElement(entry, atomElement('published'),
                 text=fmtTime(note['created_time']))
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}updated',
+        addSubElement(entry, atomElement('updated'),
                 text=fmtTime(note['updated_time']))
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}author',
+        addSubElement(entry, atomElement('author'),
                 text=name)
-        addSubElement(entry, '{http://www.w3.org/2005/Atom}content',
+        addSubElement(entry, atomElement('content'),
                 attrs={'type': 'html'},
                 text=note['content'].replace('\n', '<br/>'))
     
